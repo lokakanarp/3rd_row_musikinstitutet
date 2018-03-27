cd872ed1fbcde0320c4f1148a3c95979ddf6202c
Ida <ida.bergstrom@medieinstitutet.se>
2018-03-27 15:00:43 +0200

    Added link to style.css in index.html

--- scripts/main.js
+++ scripts/main.js
@@ -12,40 +12,34 @@ searchElement.appendChild(artistSearchButton);
 artistSearchButton.addEventListener('click', function(){
     // if artist redan finns: fortsätt till albumformular.
     // else:
-    console.log("hej");
     postArtist(artistSearchField.value);
 })
 
-
-//function postArtist(artistName){
     
 function postArtist(artistName){
-
     
     let artist = {
-    name: artistName,
-    born: 2000-12-12,
-    genres: "hej",
-    gender: "other",
-    countryBorn: "hej",
-    spotifyURL: "hej",
-    coverImage: "hej"
+        name: artistName,
+        born: 2000-12-12,
+        genres: "hej",
+        gender: "other",
+        countryBorn: "hej",
+        spotifyURL: "hej",
+        coverImage: "hej"
     }
 
 
-fetch('https://folksa.ga/api/artists?key=flat_eric',{
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(artist)
-  })
-  .then((response) => response.json())
-  .then((artist) => {
-    console.log(artist);
-  });
-    
-
+    fetch('https://folksa.ga/api/artists?key=flat_eric',{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(artist)
+      })
+      .then((response) => response.json())
+      .then((artist) => {
+        console.log(artist);
+      });
     
 }
