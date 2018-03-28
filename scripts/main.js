5333b937cab066a09717f80b452b7ef7b3fca215
Loka Kanarp <lokakanarp@gmail.com>
2018-03-28 10:51:37 +0200

    trying out posting to the API

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ artistSearchButton.addEventListener('click', function(){
 
     
 function postArtist(artistName){
-    let artist = {
+    /*let artist = {
         name: artistName,
         born: 2000-12-12,
         genres: "hej",
@@ -25,7 +25,17 @@ function postArtist(artistName){
         countryBorn: "hej",
         spotifyURL: "hej",
         coverImage: "hej"
-    }
+    }*/
+	
+	let artist = {
+    name: artistName,
+    born: "1954-09-26",
+    gender: "female",
+	countryBorn: "Italy",
+    genres: "Pop", //Must be a comma separated string
+    spotifyURL: "https://open.spotify.com/artist/3zNFrznlC0kv866J7Karl3?si=XNPUij8KQKCDnijOs959oA",
+    coverImage: "https://img.discogs.com/Y8V3M_FpSCxTMisupBlgQb9tGZM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1315032-1214684059.jpeg.jpg"
+}
 
     fetch('https://folksa.ga/api/artists?key=flat_eric',{
         method: 'POST',
@@ -42,7 +52,7 @@ function postArtist(artistName){
 }
 
 
-deleteArtist();
+//deleteArtist();
 function deleteArtist(){
     fetch(`https://folksa.ga/api/artists/5aba3d977396550e47352c8f?key=flat_eric`, {
         method: 'DELETE',
